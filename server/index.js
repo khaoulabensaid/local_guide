e5a6d3a0f56cf656a0968fb64a48ba71d37ec40b
mohamedBenAmmar <mr.weezy1312@gmail.com>
2020-07-20 10:56:05 +0100

    4

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/../client/dist"));
-
+console.log("hello");
 app.listen(3000, function () {
   console.log("listening on port 3000!");
 });
