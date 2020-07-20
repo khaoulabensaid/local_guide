a96212a3ae7ed1d358094eea7a18b484cc3514ec
mohamedBenAmmar <mr.weezy1312@gmail.com>
2020-07-20 10:49:01 +0100

    set

---
+++ database-mongo/index.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/todo");
+var db = mongoose.connection;
+
+db.on("error", function () {
+  console.log("mongoose connection error");
+});
+
+db.once("open", function () {
+  console.log("mongoose connected successfully");
+});
